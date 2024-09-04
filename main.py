4f647bed647d180f9b7579f7bcd6a9c48887f7c1
Showdown76py <showdown76.a@gmail.com>
2024-09-04 13:33:52 +0200

    robots.txt

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ def index():
 def projects():
     return render_template('projects.html')
 
+@app.route('/robots.txt')
+def robots():
+    return app.send_static_file('robots.txt')
+
 if __name__ == '__main__':         
-    print('Ready')                                                                                                                                                                                 
+    print('OK')                                                                                                                                                                                 
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT")), debug=1, threaded=True)
